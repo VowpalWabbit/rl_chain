ea816251b8bbd0b051fb9869bdaf2d7ff9ca5b1f
olgavrou <olgavrou@gmail.com>
2023-07-05 22:08:40 +0000

    cleanup

--- personalizer.py
+++ personalizer.py
@@ -8,7 +8,7 @@ from typing import Any, Dict, List, Optional
 
 from sentence_transformers import SentenceTransformer
 import vowpal_wabbit_next as vw
-from personalizer_prompt import PROMPT
+from .personalizer_prompt import PROMPT
 
 from pydantic import Extra, Field, root_validator
 import numpy as np
@@ -28,19 +28,6 @@ def parse_lines(parser: vw.TextFormatParser, input_str: str) -> List[vw.Example]
     return [parser.parse_line(line) for line in input_str.split("\n")]
 
 
-def to_vw_example_format(context_embed, actions, cb_label=None) -> str:
-    if cb_label is not None:
-        chosen_action, cost, prob = cb_label
-    example_string = ""
-    example_string += f"shared |Context {context_embed}\n"
-    for i, action in enumerate(actions):
-        if cb_label is not None and chosen_action == i:
-            example_string += "{}:{}:{} ".format(chosen_action, cost, prob)
-        example_string += "|Action {} \n".format(action)
-    # Strip the last newline
-    return example_string[:-1]
-
-
 class PersonalizerChain(Chain):
     """
     PersonalizerChain class that utilizes the Vowpal Wabbit (VW) model for personalization.
@@ -50,6 +37,8 @@ class PersonalizerChain(Chain):
         embeddings (FeatureEmbeddings, optional): The type of embeddings to be used for feature representation. Defaults to BERT.
         model_loading (bool, optional): If set to True, the chain will attempt to load an existing VW model from the latest checkpoint file in the current directory. If set to False, it will start training from scratch, potentially overwriting existing files. Defaults to True.
         actions (List[str], optional): A list of action strings for VW to choose from.
+        large_action_spaces (bool, optional): If set to True and vw_cmd has not been specified in the constructor, it will enable large action spaces
+        vw_cmd (List[str], optional): Advanced users can set the VW command line to whatever they want, as long as it is compatible with the Type that is specified (Type Enum)
 
     Notes:
         The class creates a VW model instance using the provided arguments. Before the chain object is destroyed the save_progress() function can be called. If it is called, the learned VW model is saved to a file in the current directory named `.model-<checkpoint>.vw`. Checkpoints start at 1 and increment monotonically.
@@ -97,6 +86,8 @@ class PersonalizerChain(Chain):
         vw_workspace_type: Type,
         embeddings=FeatureEmbeddings.BERT,
         model_loading=True,
+        large_action_spaces=False,
+        vw_cmd=[],
         actions: List[str] = [],
         *args,
         **kwargs,
@@ -128,22 +119,28 @@ class PersonalizerChain(Chain):
         self.next_checkpoint = next_checkpoint
         print(f"next checkpoint = {self.next_checkpoint}")
 
+        las_cmd = []
+        if large_action_spaces and not vw_cmd:
+            las_cmd = ["--large_action_space"]
+
         if vw_workspace_type == PersonalizerChain.Type.CONTEXTUAL_BANDITS:
-            vw_cmd = [
-                "--cb_explore_adf",
-                "--quiet",
-                "--interactions=AC",
-                "--coin",
-                "--squarecb",
-            ]
+            if not vw_cmd:
+                vw_cmd = las_cmd + [
+                    "--cb_explore_adf",
+                    "--quiet",
+                    "--interactions=AC",
+                    "--coin",
+                    "--squarecb",
+                ]
         elif vw_workspace_type == PersonalizerChain.Type.CONDITIONAL_CONTEXTUAL_BANDITS:
-            vw_cmd = [
-                "--ccb_explore_adf",
-                "--quiet",
-                "--interactions=AC",
-                "--coin",
-                "--squarecb",
-            ]
+            if not vw_cmd:
+                vw_cmd = las_cmd + [
+                    "--ccb_explore_adf",
+                    "--quiet",
+                    "--interactions=AC",
+                    "--coin",
+                    "--squarecb",
+                ]
         else:
             raise ValueError("No other vw types supported yet")
 
@@ -152,6 +149,7 @@ class PersonalizerChain(Chain):
         else:
             raise ValueError("No other sentence transformers supported yet")
 
+        print(f"vw command: {vw_cmd}")
         # initialize things
         self.set_actions(actions)
         if serialized_workspace:
@@ -160,15 +158,10 @@ class PersonalizerChain(Chain):
             self.workspace = vw.Workspace(vw_cmd)
 
     user_history: str = "user_history"  #: :meta private:
-    # workspace: vw.Workspace = "workspace" #: :meta private:
+
     initial_prompt_key: str = "initial_prompt"  #: :meta private:
     output_key: str = "answer"  #: :meta private:
     prompt: BasePromptTemplate = PROMPT
-
-    # sbert_model = SentenceTransformer('all-MiniLM-L6-v2')
-    latest_context_emb: List = None
-    latest_prob: float = None
-    latest_action: int = None
 
     class Config:
         """Configuration for this pydantic object."""
@@ -216,46 +209,17 @@ class PersonalizerChain(Chain):
     def _call(
         self,
         inputs: Dict[str, Any],
+        preds: Any,
         run_manager: Optional[CallbackManagerForChainRun] = None,
     ) -> Dict[str, str]:
         _run_manager = run_manager or CallbackManagerForChainRun.get_noop_manager()
         _run_manager.on_text(inputs[self.initial_prompt_key], verbose=self.verbose)
 
         init_prompt = inputs[self.initial_prompt_key]
-        user_history = inputs[self.user_history]
-        text_parser = vw.TextFormatParser(self.workspace)
-
-        context_embed = ""
-        feat = 0
-        for emb in self.sbert_model.encode(init_prompt + " " + user_history):
-            context_embed += f"{feat}:{emb} "
-            feat += 1
-        # Only supports single example per prompt
-        vw_ex = to_vw_example_format(context_embed, self.action_embeddings)
-        multi_ex = parse_lines(text_parser, vw_ex)
-
-        self.latest_context_emb = context_embed
-        preds = self.workspace.predict_one(multi_ex)
-
-        print(f"preds: {preds}")
-        prob_sum = sum(prob for _, prob in preds)
-        probabilities = [prob / prob_sum for _, prob in preds]
-
-        ## explore
-        sampled_index = np.random.choice(len(preds), p=probabilities)
-        sampled_ap = preds[sampled_index]
-        ## exploit
-        # sampled_ap = max(preds, key=lambda x : x[1])
-
-        sampled_action = sampled_ap[0]
-        self.latest_action = sampled_action
-        self.latest_prob = sampled_ap[1]
-        # winertext = actions[best_action].name + " " + actions[best_action].tags
-        winertext = self.actions[sampled_action].name
-        print("BEST MODEL SELECTION: {}".format(winertext))
+        print(f"THE PREDS I GOT: {preds}")
 
         t = self.llm_chain.predict(
-            actions=[winertext],
+            actions=[preds],
             initial_prompt=init_prompt,
             callbacks=_run_manager.get_child(),
         )
@@ -269,67 +233,6 @@ class PersonalizerChain(Chain):
         _run_manager.on_text("\nAnswer: ", verbose=self.verbose)
         _run_manager.on_text(output, color="yellow", verbose=self.verbose)
         return {self.output_key: output}
-
-    def good_recommendation(
-        self,
-        index: int,
-        inputs: Dict[str, Any],
-    ):
-        text_parser = vw.TextFormatParser(self.workspace)
-
-        # reward means the smallest cost
-        cb_label = (index, -1, self.latest_prob)
-        vw_ex = to_vw_example_format(
-            self.latest_context_emb, self.action_embeddings, cb_label
-        )
-        multi_ex = parse_lines(text_parser, vw_ex)
-        self.workspace.learn_one(multi_ex)
-
-    def neutral_recommendation(
-        self,
-        index: int,
-        inputs: Dict[str, Any],
-    ):
-        text_parser = vw.TextFormatParser(self.workspace)
-
-        # punish means the intermediate cost
-        cb_label = (index, 0, self.latest_prob)
-
-        vw_ex = to_vw_example_format(
-            self.latest_context_emb, self.action_embeddings, cb_label
-        )
-        multi_ex = parse_lines(text_parser, vw_ex)
-        self.workspace.learn_one(multi_ex)
-
-    def bad_recommendation(
-        self,
-        index: int,
-        inputs: Dict[str, Any],
-    ):
-        text_parser = vw.TextFormatParser(self.workspace)
-
-        # punish means the biggest cost
-        cb_label = (index, 1, self.latest_prob)
-
-        vw_ex = to_vw_example_format(
-            self.latest_context_emb, self.action_embeddings, cb_label
-        )
-        multi_ex = parse_lines(text_parser, vw_ex)
-        self.workspace.learn_one(multi_ex)
-
-    @property
-    def _chain_type(self) -> str:
-        return "llm_personalizer_chain"
-
-    @classmethod
-    def from_llm(
-        cls,
-        llm: BaseLanguageModel,
-        prompt: BasePromptTemplate = PROMPT,
-        **kwargs: Any,
-    ) -> PersonalizerChain:
-        llm_chain = LLMChain(llm=llm, prompt=prompt)
-        return cls(llm_chain=llm_chain, **kwargs)
 
     def save_progress(self):
         """
@@ -348,3 +251,172 @@ class PersonalizerChain(Chain):
         print(f"storing in: .model-{self.next_checkpoint}.vw")
         with open(f".model-{self.next_checkpoint}.vw", "wb") as f:
             f.write(serialized_workspace)
+
+    @property
+    def _chain_type(self) -> str:
+        return "llm_personalizer_chain"
+
+    @classmethod
+    def from_llm(
+        cls,
+        llm: BaseLanguageModel,
+        prompt: BasePromptTemplate = PROMPT,
+        **kwargs: Any,
+    ) -> ContextualBanditPersonalizerChain:
+        llm_chain = LLMChain(llm=llm, prompt=prompt)
+
+        if "vw_workspace_type" not in kwargs:
+            raise ValueError("vw_workspace_type must be specified")
+        if kwargs.get("vw_workspace_type") is PersonalizerChain.Type.CONTEXTUAL_BANDITS:
+            return ContextualBanditPersonalizerChain(llm_chain=llm_chain, **kwargs)
+        elif (
+            kwargs.get("vw_workspace_type")
+            is PersonalizerChain.Type.CONDITIONAL_CONTEXTUAL_BANDITS
+        ):
+            raise ValueError("Not implemented yet")
+        else:
+            raise ValueError("Type not supported")
+
+
+class ContextualBanditPersonalizerChain(PersonalizerChain):
+    latest_context_emb: List = None
+    latest_prob: float = None
+    latest_action: int = None
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+    def to_vw_example_format(self, context_embed, actions, cb_label=None) -> str:
+        if cb_label is not None:
+            chosen_action, cost, prob = cb_label
+        example_string = ""
+        example_string += f"shared |Context {context_embed}\n"
+        for i, action in enumerate(actions):
+            if cb_label is not None and chosen_action == i:
+                example_string += "{}:{}:{} ".format(chosen_action, cost, prob)
+            example_string += "|Action {} \n".format(action)
+        # Strip the last newline
+        return example_string[:-1]
+
+    def _call(
+        self,
+        inputs: Dict[str, Any],
+        run_manager: Optional[CallbackManagerForChainRun] = None,
+    ) -> Dict[str, str]:
+        _run_manager = run_manager or CallbackManagerForChainRun.get_noop_manager()
+        _run_manager.on_text(inputs[self.initial_prompt_key], verbose=self.verbose)
+
+        init_prompt = inputs[self.initial_prompt_key]
+        user_history = inputs[self.user_history]
+        text_parser = vw.TextFormatParser(self.workspace)
+
+        context_embed = ""
+        feat = 0
+        for emb in self.sbert_model.encode(init_prompt + " " + user_history):
+            context_embed += f"{feat}:{emb} "
+            feat += 1
+        # Only supports single example per prompt
+        vw_ex = self.to_vw_example_format(context_embed, self.action_embeddings)
+        multi_ex = parse_lines(text_parser, vw_ex)
+        self.latest_context_emb = context_embed
+        preds = self.workspace.predict_one(multi_ex)
+
+        print(f"preds: {preds}")
+        prob_sum = sum(prob for _, prob in preds)
+        probabilities = [prob / prob_sum for _, prob in preds]
+
+        ## explore
+        sampled_index = np.random.choice(len(preds), p=probabilities)
+        sampled_ap = preds[sampled_index]
+        ## exploit
+        # sampled_ap = max(preds, key=lambda x : x[1])
+
+        sampled_action = sampled_ap[0]
+        self.latest_action = sampled_action
+        self.latest_prob = sampled_ap[1]
+        # winer_text = actions[best_action].name + " " + actions[best_action].tags
+        winer_text = self.actions[sampled_action].name
+
+        return super()._call(run_manager=run_manager, inputs=inputs, preds=winer_text)
+
+    def good_recommendation(
+        self,
+        index: int,
+        inputs: Dict[str, Any],
+    ):
+        """
+        Learn will be called with a cost of -1 (cost range if good/neutural/bad are used is [-1, 1])
+
+        Note:
+            Care is needed if mixed with calling learn_with_specific_cost
+        """
+        text_parser = vw.TextFormatParser(self.workspace)
+
+        # reward means the smallest cost
+        cb_label = (index, -1, self.latest_prob)
+        vw_ex = self.to_vw_example_format(
+            self.latest_context_emb, self.action_embeddings, cb_label
+        )
+        multi_ex = parse_lines(text_parser, vw_ex)
+        self.workspace.learn_one(multi_ex)
+
+    def neutral_recommendation(
+        self,
+        index: int,
+        inputs: Dict[str, Any],
+    ):
+        """
+        Learn will be called with a cost of 0 (cost range if good/neutural/bad are used is [-1, 1])
+
+        Note:
+            Care is needed if mixed with calling learn_with_specific_cost
+        """
+        text_parser = vw.TextFormatParser(self.workspace)
+
+        # punish means the intermediate cost
+        cb_label = (index, 0, self.latest_prob)
+
+        vw_ex = self.to_vw_example_format(
+            self.latest_context_emb, self.action_embeddings, cb_label
+        )
+        multi_ex = parse_lines(text_parser, vw_ex)
+        self.workspace.learn_one(multi_ex)
+
+    def bad_recommendation(
+        self,
+        index: int,
+        inputs: Dict[str, Any],
+    ):
+        """
+        Learn will be called with a cost of 1 (cost range if good/neutural/bad are used is [-1, 1])
+
+        Note:
+            Care is needed if mixed with calling learn_with_specific_cost
+        """
+        text_parser = vw.TextFormatParser(self.workspace)
+
+        # punish means the biggest cost
+        cb_label = (index, 1, self.latest_prob)
+
+        vw_ex = self.to_vw_example_format(
+            self.latest_context_emb, self.action_embeddings, cb_label
+        )
+        multi_ex = parse_lines(text_parser, vw_ex)
+        self.workspace.learn_one(multi_ex)
+
+    def learn_with_specific_cost(self, index: int, inputs: Dict[str, Any], cost: int):
+        """
+        Learn will be called with the cost specified
+
+        Note:
+            Care is needed if mixed with calling good/bad/neutural_reccommendation methods
+        """
+        text_parser = vw.TextFormatParser(self.workspace)
+
+        cb_label = (index, cost, self.latest_prob)
+
+        vw_ex = self.to_vw_example_format(
+            self.latest_context_emb, self.action_embeddings, cb_label
+        )
+        multi_ex = parse_lines(text_parser, vw_ex)
+        self.workspace.learn_one(multi_ex)
