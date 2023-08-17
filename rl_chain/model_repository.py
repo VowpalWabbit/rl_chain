da19c5f5318844d064f50713810d4841e4e5c6ff
Alexey Taymanov <ataymano@microsoft.com>
2023-08-17 10:16:00 -0400

    do not cleanup folder

--- rl_chain/model_repository.py
+++ rl_chain/model_repository.py
@@ -4,6 +4,7 @@ import datetime
 import vowpal_wabbit_next as vw
 from typing import Union, Sequence
 import os
+import glob
 import logging
 
 class ModelRepository:
@@ -11,19 +12,25 @@ class ModelRepository:
         self.folder = Path(folder)
         self.model_path = self.folder / "latest.vw"
         self.with_history = with_history
-        if reset and self.folder.exists():
-            shutil.rmtree(self.folder)
+        if reset and self.has_history:
+            logger.warning('There is non empty history which is recommended to be cleaned up')
+            if self.model_path.exists():
+                os.remove(self.model_path)
+
         self.folder.mkdir(parents=True, exist_ok=True)
         self.logger = logger
 
     def get_tag(self) -> str:
         return datetime.datetime.now().strftime("%Y%m%d-%H%M%S")
+    
+    def has_history(self) -> bool:
+        return len(glob.glob(str(self.folder / 'model-????????-??????.vw'))) > 0
 
     def save(self, workspace: vw.Workspace) -> None:
         with open(self.model_path, "wb") as f:
             self.logger.info(f"storing rl_chain model in: {self.model_path}")
             f.write(workspace.serialize())
-        if self.with_history:
+        if self.with_history: # write history
             shutil.copyfile(self.model_path, self.folder / f"model-{self.get_tag()}.vw")
 
     def load(self, commandline: Sequence[str]) -> vw.Workspace:
